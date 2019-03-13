8ce6dedacf2e2a28385a5243befabe294e556b18
Abasienyene <abasonform@gmail.com>
2019-03-13 15:54:51 +0100

    Initial commit feed back app ui page first trial

---
+++ app/src/main/java/com/example/feedback/DetailedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.feedback
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed)
+    }
+}
